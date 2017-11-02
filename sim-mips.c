a6d03492d20384f623502149361c77ee73d5e359
Unknown <jmuri@umass.edu>
2017-11-02 14:41:37 -0400

    code3

--- sim-mips.c
+++ sim-mips.c
@@ -111,6 +111,24 @@ main (int argc, char *argv[]){
 			mips_reg[i]=0;
 		}
 	}
+	else if(sim_mode==0){
+		fprintf(output,"program name: %s\n",argv[5]);
+		fprintf(output,"stage utilization: %f  %f  %f  %f  %f \n",
+                             ifUtil, idUtil, exUtil, memUtil, wbUtil);
+                     // add the (double) stage_counter/sim_cycle for each 
+                     // stage following sequence IF ID EX MEM WB
+		
+		fprintf(output,"register values ");
+		for (i=1;i<REG_NUM;i++){
+			fprintf(output,"%d  ",mips_reg[i]);
+		}
+		fprintf(output,"%d\n",pgm_c);
+	
+	}
+	//close input and output files at the end of the simulation
+	fclose(input);
+	fclose(output);
+	return 0;
 
 	//Jeff muri likes spicy asian food
 	//Jackson like whaners
